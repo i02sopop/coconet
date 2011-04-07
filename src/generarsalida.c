1ac2802a4b4a352873152bd7bdcab121bb3bab0c
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2011-04-07 17:42:52 +0200

    Updated the copyright statement and the license statement (we use now GPLv3 instead GPLv2)

--- src/generarsalida.c
+++ src/generarsalida.c
@@ -1,22 +1,20 @@
-/*******************************************************************************
-*Copyright 2005 Pablo Alvarez de Sotomayor Posadillo			       *
-*									       *
-*This file is part of Coconet.						       *
-*									       *
-*Coconet is free software; you can redistribute it and/or modify	       *
-*it under the terms of the GNU General Public License as published by	       *
-*the Free Software Foundation; either version 2 of the License, or	       *
-*(at your option) any later version.					       *
-*									       *
-*Coconet is distributed in the hope that it will be useful,		       *
-*but WITHOUT ANY WARRANTY; without even the implied warranty of		       *
-*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		       *
-*GNU General Public License for more details.				       *
-*									       *
-*You should have received a copy of the GNU General Public License	       *
-*along with Coconet; if not, write to the Free Software			       *
-*Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA     *
-*******************************************************************************/
+/********************************************************************************
+* Copyright (c) 2004-2011 coconet project (see AUTHORS)			        *
+*									        *
+* This file is part of Coconet.						        *
+*									        *
+* Coconet is free software: you can redistribute it and/or modify it under the  *
+* terms of the GNU General Public License as published by the Free Software     *
+* Foundation, either version 3 of the License, or (at your option) any later    *
+* version.                                                                      *
+*									        *
+* Coconet is distributed in the hope that it will be useful, but WITHOUT ANY    *
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR *
+* A PARTICULAR PURPOSE.  See the GNU General Public License for more details.   *
+*									        *
+* You should have received a copy of the GNU General Public License along with  *
+* coconet. If not, see <http://www.gnu.org/licenses/>.                          *
+********************************************************************************/
 
 #include <definiciones.h>
 
